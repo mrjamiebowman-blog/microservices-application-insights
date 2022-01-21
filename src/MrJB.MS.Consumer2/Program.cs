4984f3729c70cb01fbb4f5d1c29dd28581383c99
Jamie Bowman <mrjamiebowman@gmail.com>
2022-01-20 20:29:21 -0500

    set up consumers/workers for other consumer services.

--- src/MrJB.MS.Consumer2/Program.cs
+++ src/MrJB.MS.Consumer2/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.ApplicationInsights.WorkerService;
+using MrJB.MS.Common.Configuration;
+using MrJB.MS.Common.Extensions;
 using MrJB.MS.Consumer2;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((builder, services) =>
     {
+        // app insights
+        var options = new ApplicationInsightsServiceOptions();
+        options.EnableHeartbeat = true;
+        options.InstrumentationKey = builder.Configuration.GetValue<string>($"{AppInsightsConfiguration.Position}:{nameof(AppInsightsConfiguration.InstrumentationKey)}");
+        services.AddApplicationInsightsTelemetryWorkerService(options);
+        services.AddCustomApplicationInsightsWorker(builder.Configuration);
+
+        services.AddAzureServiceBusConsumerConfiguration(builder.Configuration);
+        services.AddAzureServiceBusProducerConfiguration(builder.Configuration);
         services.AddHostedService<Worker>();
     })
     .Build();
